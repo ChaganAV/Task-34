5f3f22541d051e89edd213764b49e352402783fa
Alexandr <jwchaganav@gmail.com>
2022-11-11 14:27:28 +0400

    End

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int size = 15;
+int[] arrNum = new int[size];
+int min = 100;
+int max = 1000;
+int countPositive = 0;
+Random rnd = new Random();
+for (int i = 0; i < arrNum.Length; i++)
+{
+    arrNum[i] = rnd.Next(min,max);
+}
+PrintArray(arrNum);
+
+for(int i = 0; i < arrNum.Length; i++)
+{
+    if(arrNum[i]%2 == 0)
+        countPositive++;
+}
+Console.WriteLine($"Количество четных чисел {countPositive}");
+
+// Functions
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
